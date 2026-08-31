225c8d4ff792cd1f0b43fee4c4cbe68fedea5805
Yannick Couzinie <couzinie@axelspace.com>
2026-09-01 08:43:07 +0900

    Add snafu based error handling

--- src/numerical/integrate/errors.rs
+++ src/numerical/integrate/errors.rs
@@ -1,24 +1,22 @@
 use nalgebra::SVector;
+use snafu::Snafu;
 
-#[derive(Debug)]
+#[derive(Debug, Snafu)]
 pub enum IntegrationError<const N: usize> {
-    InvalidStepSize {
-        step: f64,
-    },
-    InvalidTimeSpan {
-        start: f64,
-        end: f64,
-    },
-    StepDoesNotAdvanceTime {
-        time: f64,
-        step: f64,
-    },
+    #[snafu(display("invalid integration step size: {step}"))]
+    InvalidStepSize { step: f64 },
+    #[snafu(display("invalid integration time span: {start} to {end}"))]
+    InvalidTimeSpan { start: f64, end: f64 },
+    #[snafu(display("invalid integration step (does not advance time): {time} + {step}"))]
+    StepDoesNotAdvanceTime { time: f64, step: f64 },
+    #[snafu(display(
+        "derivative is non-finite: derivative={derivative}, state={state}, time={time}"
+    ))]
     NonFiniteDerivative {
         derivative: SVector<f64, N>,
         state: SVector<f64, N>,
         time: f64,
     },
-    NonFiniteState {
-        state: SVector<f64, N>,
-    },
+    #[snafu(display("state is non-finite: state={state}"))]
+    NonFiniteState { state: SVector<f64, N> },
 }
